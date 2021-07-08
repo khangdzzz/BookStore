0e5cda9e880112f2737b871e810d8dc1161d2477
KhangDoVan <dvk10102000@gmail.com>
2021-07-08 16:08:08 +0700

    update file index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function Cat(name){
+    this.name = name;
+    this.stomach = [];
+}
+
+Cat.prototype.eat = function(){
+    this.stomach.push('mouse1');
+}
+
+function Mouse(){
+    this.dead = true;
+}
